73ec25dec84aeda9746442ab5e39f488eb684428
ufuk halis <ufukhalis@gmail.com>
2021-01-30 11:08:47 +0100

    Add on cache hit and miss methods and change structure.

--- src/main/java/io/github/ufukhalis/defuk/DefukCache.java
+++ src/main/java/io/github/ufukhalis/defuk/DefukCache.java
@@ -4,9 +4,17 @@ import io.github.ufukhalis.defuk.adapter.DefukCacheAdapter;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public final class DefukCache {
+public class DefukCache<K, V> {
+
+    private final Supplier<CompletableFuture<V>> completableFutureSupplier;
+    private final K key;
+    private final DefukCacheAdapter<K, V> adapter;
+
+    private Consumer<? super V> onHit;
+    private Consumer<K> onMiss;
 
     public static <T, M> T fromCache(final Supplier<T> operation, final M key, final DefukCacheAdapter<M, T> adapter) {
         Optional<T> maybeResult = adapter.get(key);
@@ -20,20 +28,47 @@ public final class DefukCache {
         return result;
     }
 
+    public DefukCache(Supplier<CompletableFuture<V>> completableFutureSupplier, K key, DefukCacheAdapter<K, V> adapter) {
+        this.completableFutureSupplier = completableFutureSupplier;
+        this.key = key;
+        this.adapter = adapter;
+    }
 
-    public static <T, M> CompletableFuture<T> fromNonBlockingCache(final Supplier<CompletableFuture<T>> completableFutureSupplier,
-                                                                   final M key,
-                                                                   final DefukCacheAdapter<M, T> adapter) {
+    public static <T, M> DefukCache<T, M> fromNonBlockingCache(final Supplier<CompletableFuture<M>> completableFutureSupplier,
+                                                               final T key,
+                                                               final DefukCacheAdapter<T, M> adapter) {
+        return new DefukCache<>(completableFutureSupplier, key, adapter);
+    }
 
-        Optional<T> maybeResult = adapter.get(key);
+    public final DefukCache<K, V> doOnCacheHit(Consumer<? super V> onHit) {
+        this.onHit = onHit;
+        return this;
+    }
 
-        return maybeResult.map(CompletableFuture::completedFuture)
+    public final DefukCache<K, V> doOnCacheMiss(Consumer<K> onMiss) {
+        this.onMiss = onMiss;
+        return this;
+    }
+
+    public final CompletableFuture<V> subscribe() {
+        Optional<V> maybeResult = adapter.get(key);
+
+        return maybeResult
+                .map(__ -> executeConsumerFunction(onHit, __))
+                .map(CompletableFuture::completedFuture)
                 .orElseGet(() -> completableFutureSupplier.get()
                         .thenApplyAsync(value -> {
+                            executeConsumerFunction(onMiss, key);
                             adapter.put(key, value);
                             return value;
                         })
                 );
+    }
 
+    private <T> T executeConsumerFunction(Consumer<? super T> consumer, T o) {
+        if (consumer != null) {
+            consumer.accept(o);
+        }
+        return o;
     }
 }
--- src/test/java/io/github/ufukhalis/defuk/DefukCacheTests.java
+++ src/test/java/io/github/ufukhalis/defuk/DefukCacheTests.java
@@ -2,6 +2,7 @@ package io.github.ufukhalis.defuk;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import io.github.ufukhalis.defuk.DefukCache;
 import io.github.ufukhalis.defuk.adapter.CaffeineCacheAdapter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DefukCacheTests {
 
@@ -33,17 +35,30 @@ public class DefukCacheTests {
         Assertions.assertEquals(cacheResult, cacheValue.get());
     }
 
+
     @Test
     void whenFromNonBlockingCache_thenShouldCacheValue() throws ExecutionException, InterruptedException {
-        String key = "defuk";
+        String key = "defuk-nonblocking";
         Assertions.assertFalse(adapter.get(key).isPresent());
 
-        Integer cacheResult = DefukCache
-                .fromNonBlockingCache(() -> CompletableFuture.completedFuture(1), key, adapter).get();
+        AtomicInteger cacheHit = new AtomicInteger(0);
+        AtomicInteger cacheMissed = new AtomicInteger(0);
+
+        Integer cacheResult = 0;
+        for (int i = 0; i < 2; i++) {
+            cacheResult = DefukCache
+                    .fromNonBlockingCache(() -> CompletableFuture.completedFuture(1), key, adapter)
+                    .doOnCacheMiss(__ -> cacheMissed.incrementAndGet())
+                    .doOnCacheHit(__ -> cacheHit.incrementAndGet())
+                    .subscribe()
+                    .get();
+        }
 
         Optional<Integer> cacheValue = adapter.get(key);
 
         Assertions.assertTrue(cacheValue.isPresent());
         Assertions.assertEquals(cacheResult, cacheValue.get());
+        Assertions.assertEquals(1, cacheHit.get());
+        Assertions.assertEquals(1, cacheMissed.get());
     }
 }
